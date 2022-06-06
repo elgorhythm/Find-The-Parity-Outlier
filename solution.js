515bbf74e3b6fbdfc8151665e341bfcfd53ec409
Adnan Ather <99840210+elgorhythm@users.noreply.github.com>
2022-06-06 02:21:39 -0600

    Update solution.js

--- solution.js
+++ solution.js
@@ -21,3 +21,12 @@ function findOutlier(integers){
     }
   }
 }
+
+
+#Alternate
+
+function findOutlier(integers){
+  let even = integers.filter(a=>a%2==0);
+  let odd = integers.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}
