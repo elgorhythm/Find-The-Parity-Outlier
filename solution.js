b6cd096745bdf57eae2d885967ac5ff8008d03cc
Adnan Ather <99840210+elgorhythm@users.noreply.github.com>
2022-06-06 02:09:54 -0600

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,23 @@
-#solution code
+function findOutlier(integers){
+  let oddCount=0
+  let evenCount=0
+  for (i=0; i<integers.length; i++) {
+    if (integers[i]%2===0) {
+      evenCount ++
+    }     
+  }
+  
+  oddCount = integers.length - evenCount
+  
+  if(oddCount>evenCount) {
+    for(i=0; i<integers.length; i++) {
+      if(integers[i]%2===0) return integers[i]
+    }
+  }
+  else {
+    for(i=0; i<integers.length; i++) {
+      if(integers[i]%2!==0) 
+      {return integers[i]}
+    }
+  }
+}
